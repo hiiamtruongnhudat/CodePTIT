07faa37f1657cc3c756ec5d7640a5bcd1af63063
hiiamtruongnhudat <dattn.b19at039@stu.ptit.edu.vn>
2021-06-12 10:20:43 +0700

    first day code

--- LuyThuaDao/main.cpp
+++ LuyThuaDao/main.cpp
@@ -2,17 +2,32 @@
 using namespace std;
 const long long mod = 1e9+7;
 
-int Pow(int x, int n)
+long long Pow(long long x, long long n)
 {
     if (n==1)
         return x%mod;
-    int temp = Pow(x,n/2);
-    return (n&1) ? temp*temp*x : temp*temp;
+    long long tmp = Pow(x,n/2);
+    return (n&1) ?  (tmp*tmp%mod)* x % mod : (tmp * tmp % mod);
 }
-
+long long reverseNumber(long long n)
+{   
+    long long rem, reverse =0;
+    while(n!=0)    
+    {    
+        rem=n%10;      
+        reverse=reverse*10+rem;    
+        n/=10;    
+    } 
+    return reverse;
+}
 int main()
 {
-    int a = 5, n = 2;
-    cout << Pow(5,1);
+    int t; cin >> t;
+    while ( t-- )
+    {
+        long long n; cin >> n;
+        long long k = reverseNumber(n);
+        cout << Pow(n,k) << "\n";
+    }
     return 0;
 }
